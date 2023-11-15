5e3b361ddd77b9b5ec7450843b985138a777793a
manm2069 <manm2069@gmail.com>
2023-11-15 14:40:48 -0500

    Added actor.py

---
+++ actor.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+# Set up the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Define and intialize pins
+led_pin = 18
+GPIO.setup(led_pin, GPIO.OUT)
+
+# Define the MQTT broker and topic
+broker_address = "localhost"  # Replace with the Raspberry Pi's IP if not running locally
+topic = "tank/water"  # Replace with the desired MQTT topic
+
+# Callback functions for MQTT client
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code " + str(rc))
+    client.subscribe(topic)
+
+def on_message(client, userdata, message):
+    print(f"Received message on topic '{message.topic}': {message.payload.decode()}")
+    client.open_tank()
+
+def open_tank():
+    try:
+        GPIO.output(led_pin, 1)
+
+    finally:
+        GPIO.cleanup()
+
+# Create an MQTT client
+client = mqtt.Client()
+
+# Set up callback functions
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to the MQTT broker
+client.connect(broker_address)
+
+# Start the MQTT client loop to receive messages
+client.loop_forever()
